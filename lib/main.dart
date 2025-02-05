88d48d52013c664ebabbc5e839b433ea94ccf1f3
AMarNaUndartaker <settdragon@gmail.com>
2025-02-05 15:38:34 +0700

    upmain

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,83 @@
-import 'package:adidas/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'home_screen.dart'; // นำเข้าไฟล์ HomePage
 
 void main() {
-  runApp(AdidasApp());
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
 }
 
-class AdidasApp extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Adidas Store',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Roboto',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SHOP'),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        elevation: 4,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              print("Profile icon tapped");
+            },
+          ),
+        ],
       ),
-      home: HomePage(), // เรียกใช้หน้าหลัก
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search...",
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                SizedBox(width:10),
+                Text(
+                  "Men",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+                SizedBox(width:10),
+                Text(
+                  "Women",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+                SizedBox(width:10),
+                Text(
+                  "Kids",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child:Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 255, 255, 255),
+                  Color.fromARGB(255, 255, 255, 255),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ), )
+          
+        ],
+      ),
     );
   }
 }
