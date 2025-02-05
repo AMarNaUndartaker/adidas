4fffd6acf3bad0b2e68a6c28b7a48195798f7174
AMarNaUndartaker <settdragon@gmail.com>
2025-02-05 15:09:29 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
-}
-
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
